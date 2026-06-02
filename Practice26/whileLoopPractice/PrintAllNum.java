8d94a5591af8b54631f9049d0681563c3ecdc0d8
Masoom Raza <masoomraza843113@outlook.com>
2026-06-02 17:37:41 +0530

    Added a folder while loop- practice

---
+++ Practice26/whileLoopPractice/PrintAllNum.java
@@ -0,0 +1,10 @@
+package whileLoopPractice;
+public class PrintAllNum{
+    public static void main(String... arg){
+        int start = 1;
+        while(start <= 10){
+            System.out.println("Numbers Is Printed from " + start +" to 10");
+            start++;
+        }
+    }
+}
